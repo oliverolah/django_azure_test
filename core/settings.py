ac0976fc1bcc6748ab7b5a9449e648729f6f26a6
oliverOlah <oliverolah13@gmail.com>
2022-06-27 08:53:44 +0200

    changin settings.py apps name

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic', # Whitenoise overriding Django's staticfiles functionality
     'django.contrib.staticfiles',
-    'main', # App
+    'main.apps.MainConfig', # App
 ]
 
 MIDDLEWARE = [
